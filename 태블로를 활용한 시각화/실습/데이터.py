546668776aa1a4466e8f76e44fe6656116871d4c
RaneeKim <raneek0326@gmail.com>
2024-04-18 17:53:52 +0900

    0418수업 및 실습ing

--- 태블로를 활용한 시각화/실습/데이터.py
+++ 태블로를 활용한 시각화/실습/데이터.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 os.getcwd()
 
-os.chdir("C:\\Users\\MSI\\Desktop\\study\\SW_CAMP\\태블로를 활용한 시각화\\실습")
+os.chdir(r"C:\Users\UserK\Desktop\Ranee\SW_CAMP\태블로를 활용한 시각화\실습")
 
 mainn = pd.read_csv("서울시 상권분석서비스(추정매출-상권).csv", encoding='cp949')
 namee = pd.read_csv("서울시 상권분석서비스(영역-상권).csv", encoding='cp949')
@@ -56,3 +56,28 @@ result.head()
 result.tail()
 
 result.to_csv('최종파일.csv',encoding='cp949')
+
+
+result = pd.read_csv("최종파일.csv", encoding='cp949')
+
+
+
+df1 = result.melt(id_vars = ['기준_년분기_코드', '자치구_코드_명', '서비스_업종_코드_명', '당월_매출_금액', '당월_매출_건수', '주중_매출_금액', '주말_매출_금액', 
+                                 '시간대_00~06_매출_금액', '시간대_06~11_매출_금액', '시간대_11~14_매출_금액', '시간대_14~17_매출_금액', '시간대_17~21_매출_금액', 
+                                 '시간대_21~24_매출_금액', '남성_매출_금액', '여성_매출_금액', '연령대_10_매출_금액', '연령대_20_매출_금액', 
+                                 '연령대_30_매출_금액', '연령대_40_매출_금액', '연령대_50_매출_금액', '연령대_60_이상_매출_금액'] ,
+                        value_vars=['월요일_매출_금액', '화요일_매출_금액', '수요일_매출_금액', '목요일_매출_금액', '금요일_매출_금액', '토요일_매출_금액', '일요일_매출_금액'],
+                        var_name='요일',
+                        value_name='요일별_매출_금액')
+
+result.info()
+df1.info()
+
+
+df1.columns.to_list()
+
+df1.to_csv('최종파일2.csv',encoding='cp949')
+
+
+df2 = df1.melt(id_vars = ['기준_년분기_코드', '자치구_코드_명', '서비스_업종_코드_명', '당월_매출_금액', '당월_매출_건수', '주중_매출_금액', '주말_매출_금액', 
+                          )
