cf6d3d272e3aa43a39482c6bc0571363f7474f71
RaneeKim <raneek0326@gmail.com>
2024-04-23 17:38:43 +0900

    sql첫날

---
+++ 데이터베이스(SQL)/source/혼공SQL_예제 파일/0423수업실습.sql
@@ -0,0 +1,95 @@
+DROP DATABASE IF EXISTS market_db; -- 만약 market_db가 존재하면 우선 삭제한다.
+CREATE DATABASE market_db;
+
+USE market_db;
+CREATE TABLE member -- 회원 테이블
+( mem_id  		CHAR(8) NOT NULL PRIMARY KEY, -- 사용자 아이디(PK)
+  mem_name    	VARCHAR(10) NOT NULL, -- 이름
+  mem_number    INT NOT NULL,  -- 인원수
+  addr	  		CHAR(2) NOT NULL, -- 지역(경기,서울,경남 식으로 2글자만입력)
+  phone1		CHAR(3), -- 연락처의 국번(02, 031, 055 등)
+  phone2		CHAR(8), -- 연락처의 나머지 전화번호(하이픈제외)
+  height    	SMALLINT,  -- 평균 키
+  debut_date	DATE  -- 데뷔 일자
+);
+CREATE TABLE buy -- 구매 테이블
+(  num 		INT AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 순번(PK)
+   mem_id  	CHAR(8) NOT NULL, -- 아이디(FK)
+   prod_name 	CHAR(6) NOT NULL, --  제품이름
+   group_name 	CHAR(4)  , -- 분류
+   price     	INT  NOT NULL, -- 가격buy
+   amount    	SMALLINT  NOT NULL, -- 수량
+   FOREIGN KEY (mem_id) REFERENCES member(mem_id)
+);
+
+INSERT INTO member VALUES('TWC', '트와이스', 9, '서울', '02', '11111111', 167, '2015.10.19');
+INSERT INTO member VALUES('BLK', '블랙핑크', 4, '경남', '055', '22222222', 163, '2016.08.08');
+INSERT INTO member VALUES('WMN', '여자친구', 6, '경기', '031', '33333333', 166, '2015.01.15');
+INSERT INTO member VALUES('OMY', '오마이걸', 7, '서울', NULL, NULL, 160, '2015.04.21');
+INSERT INTO member VALUES('GRL', '소녀시대', 8, '서울', '02', '44444444', 168, '2007.08.02');
+INSERT INTO member VALUES('ITZ', '잇지', 5, '경남', NULL, NULL, 167, '2019.02.12');
+INSERT INTO member VALUES('RED', '레드벨벳', 4, '경북', '054', '55555555', 161, '2014.08.01');
+INSERT INTO member VALUES('APN', '에이핑크', 6, '경기', '031', '77777777', 164, '2011.02.10');
+INSERT INTO member VALUES('SPC', '우주소녀', 13, '서울', '02', '88888888', 162, '2016.02.25');
+INSERT INTO member VALUES('MMU', '마마무', 4, '전남', '061', '99999999', 165, '2014.06.19');
+
+INSERT INTO buy VALUES(NULL, 'BLK', '지갑', NULL, 30, 2);
+INSERT INTO buy VALUES(NULL, 'BLK', '맥북프로', '디지털', 1000, 1);
+INSERT INTO buy VALUES(NULL, 'APN', '아이폰', '디지털', 200, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '아이폰', '디지털', 200, 5);
+INSERT INTO buy VALUES(NULL, 'BLK', '청바지', '패션', 50, 3);
+INSERT INTO buy VALUES(NULL, 'MMU', '에어팟', '디지털', 80, 10);
+INSERT INTO buy VALUES(NULL, 'GRL', '혼공SQL', '서적', 15, 5);
+INSERT INTO buy VALUES(NULL, 'APN', '혼공SQL', '서적', 15, 2);
+INSERT INTO buy VALUES(NULL, 'APN', '청바지', '패션', 50, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '지갑', NULL, 30, 1);
+INSERT INTO buy VALUES(NULL, 'APN', '혼공SQL', '서적', 15, 1);
+INSERT INTO buy VALUES(NULL, 'MMU', '지갑', NULL, 30, 4);
+
+SELECT * FROM member limit 3;
+SELECT * FROM buy;
+
+-- 맴버 인원수가 4명인 데이터
+SELECT * FROM member
+where mem_number = 4;
+
+-- 맴버 인원수가 4명인 데이터 조회하되 이름,인원수,주소만 조회
+select mem_name, mem_number, addr
+from member
+where mem_number = 4;
+
+-- 키가 160이상인 데이터
+select * from member
+where height >= 160;
+
+-- 키가 160이상이고, 맴버수가 5명 초과인 데이터
+select * from member
+where height >= 160 and mem_number > 5;
+
+-- 키가 163 이상 165이하인 데이터
+select * from member
+where height >= 163 and height <= 165;
+select * from member
+where height between 163 and 165;
+
+-- 주소가 전남 또는 경기 또는 경남인 데이터
+select * from member
+where addr = '전남' or addr = '경기' or addr = '경남';
+select * from member
+where addr IN ('전남','경기','경남');
+
+-- 찾을값을 정확히 모를때
+select * from member
+where mem_name like '블랙%';
+
+-- 찾을값의 글자수만 알때
+select * from member
+where mem_name like '__핑크'; 
+
+-- mem_id에 M이 포함된 회원 조회
+select * from member
+where mem_id like '%M%';
+
+-- mem_id에서 두번째 글자가 M인 회원 조회
+select * from member
+where mem_id like '_M%';
